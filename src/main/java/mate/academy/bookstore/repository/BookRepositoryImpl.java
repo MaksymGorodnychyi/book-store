f374493d965a2b553661b7ff15217bdb823a8c9a
Maksym <pozetiv13@gmail.com>
2026-08-20 15:22:54 +0300

    Add Book entity with repository and service

---
+++ src/main/java/mate/academy/bookstore/repository/BookRepositoryImpl.java
@@ -0,0 +1,50 @@
+package mate.academy.bookstore.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import java.util.List;
+import mate.academy.bookstore.model.Book;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final EntityManagerFactory entityManagerFactory;
+
+    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save book to DB: " + book, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("From Book", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books from DB", e);
+        }
+
+    }
+}
