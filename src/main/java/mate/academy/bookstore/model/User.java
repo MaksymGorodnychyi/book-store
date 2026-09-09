0123ee04434b512234501eb73a9abdd3e1afd9c1
Maksym <pozetiv13@gmail.com>
2026-09-09 15:16:18 +0300

    Add user-role relation and users_roles table

--- src/main/java/mate/academy/bookstore/model/User.java
+++ src/main/java/mate/academy/bookstore/model/User.java
@@ -2,10 +2,16 @@ package mate.academy.bookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -31,6 +37,14 @@ public class User {
 
     @Column(nullable = false)
     private String lastName;
-    
+
     private String shippingAddress;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
