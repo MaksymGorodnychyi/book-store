8edbec73fde53337f23554e11e8b30e0ba076d63
Maksym <pozetiv13@gmail.com>
2026-09-09 16:41:15 +0300

    Encode password and assign default role on registration

--- src/main/java/mate/academy/bookstore/service/UserServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package mate.academy.bookstore.service;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.UserRegistrationRequestDto;
 import mate.academy.bookstore.dto.UserResponseDto;
 import mate.academy.bookstore.exception.RegistrationException;
 import mate.academy.bookstore.mapper.UserMapper;
+import mate.academy.bookstore.model.Role;
+import mate.academy.bookstore.model.RoleName;
 import mate.academy.bookstore.model.User;
+import mate.academy.bookstore.repository.RoleRepository;
 import mate.academy.bookstore.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +19,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -23,6 +30,11 @@ public class UserServiceImpl implements UserService {
                     + requestDto.getEmail());
         }
         User user = userMapper.toModel(requestDto);
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() ->
+                        new RegistrationException("Can't find role: " + RoleName.ROLE_USER));
+        user.setRoles(Set.of(userRole));
         User savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }
