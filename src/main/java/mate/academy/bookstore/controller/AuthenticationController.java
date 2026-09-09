a934ec86e8d627e9dcffdfec6629265261f1dd29
Maksym <pozetiv13@gmail.com>
2026-09-09 12:54:31 +0300

    Add Swagger annotations to AuthenticationController

--- src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
+++ src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package mate.academy.bookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.UserRegistrationRequestDto;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication management",
+        description = "Endpoints for user authentication and registration")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
 
+    @Operation(summary = "Register new user",
+            description = "Creates a new user account with the provided registration details")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
