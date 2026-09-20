3184837ed4978213a7e3f5841562cca528240660
Maksym <pozetiv13@gmail.com>
2026-09-20 14:57:41 +0300

    Add shopping cart DTOs

---
+++ src/main/java/mate/academy/bookstore/dto/CreateCartItemRequestDto.java
@@ -0,0 +1,16 @@
+package mate.academy.bookstore.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateCartItemRequestDto {
+    @NotNull
+    private Long bookId;
+
+    @Min(1)
+    private int quantity;
+}
